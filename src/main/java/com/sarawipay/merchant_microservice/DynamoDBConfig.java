b121f51570487f413cc15dd6ab8841133a042fa1
sara-lopez <sara.lopez@petroprix.com>
2025-04-01 14:14:18 +0200

    feat: [CONFIG]: Configuración DynamoDB

---
+++ src/main/java/com/sarawipay/merchant_microservice/DynamoDBConfig.java
@@ -0,0 +1,36 @@
+package com.sarawipay.merchant_microservice;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@RequiredArgsConstructor
+
+public class DynamoDBConfig {
+
+    @Bean
+    public AmazonDynamoDB amazonDynamoDB() {
+        return AmazonDynamoDBClientBuilder.standard()
+                .withEndpointConfiguration(
+                        new AwsClientBuilder.EndpointConfiguration(
+                                "http://localhost:8000",
+                                "us-east-1"))
+                .withCredentials(new AWSStaticCredentialsProvider(
+                        new BasicAWSCredentials("dummy", "dummy")))
+                .build();
+    }
+
+    @Bean
+    public DynamoDBMapper dynamoDBMapper(AmazonDynamoDB amazonDynamoDB) {
+        return new DynamoDBMapper(amazonDynamoDB);
+    }
+}
