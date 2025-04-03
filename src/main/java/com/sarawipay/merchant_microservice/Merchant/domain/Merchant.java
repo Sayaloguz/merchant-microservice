a19a1da1c71048e717a21d28a7b1d30a29111bc1
sara-lopez <sara.lopez@petroprix.com>
2025-04-03 13:24:53 +0200

    fix: [ENTIDADES]: Corregir anotaciones

--- src/main/java/com/sarawipay/merchant_microservice/Merchant/domain/Merchant.java
+++ src/main/java/com/sarawipay/merchant_microservice/Merchant/domain/Merchant.java
@@ -1,21 +1,23 @@
 package com.sarawipay.merchant_microservice.Merchant.domain;
 
-
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
 import com.sarawipay.merchant_microservice.Merchant.domain.enums.MerchantType;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+
 @Setter
 @Getter
-@RequiredArgsConstructor
-@DynamoDBTable(tableName = "MainTable")
-public class Merchant extends MainTable{
+@AllArgsConstructor
+@NoArgsConstructor
+public class Merchant extends MainTable {
+
     @DynamoDBAttribute(attributeName = "name")
     private String name;
+
     @DynamoDBAttribute(attributeName = "address")
     private String address;
+
     @DynamoDBAttribute(attributeName = "merchantType")
     private MerchantType merchantType;
+
+
 }
