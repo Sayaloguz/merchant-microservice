ca3b2b6f80490c0ea11eda83d5d80c744e9bdf38
sara-lopez <sara.lopez@petroprix.com>
2025-04-04 11:11:23 +0200

    feat: [SERVICE]: Implementar búsquedas por id y por nombre

--- src/main/java/com/sarawipay/merchant_microservice/Merchant/application/MerchantGetUseCaseImpl.java
+++ src/main/java/com/sarawipay/merchant_microservice/Merchant/application/MerchantGetUseCaseImpl.java
@@ -1,4 +1,52 @@
 package com.sarawipay.merchant_microservice.Merchant.application;
 
-public class MerchantGetUseCaseImpl {
+import com.sarawipay.merchant_microservice.Merchant.application.port.MerchantGetUseCase;
+import com.sarawipay.merchant_microservice.Merchant.domain.Merchant;
+import com.sarawipay.merchant_microservice.Merchant.domain.mappers.MerchantMappers;
+import com.sarawipay.merchant_microservice.Merchant.infrastructure.controller.DTO.output.MerchantOutputDTO;
+import com.sarawipay.merchant_microservice.Merchant.infrastructure.controller.repository.port.MerchantRepository;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Service
+public class MerchantGetUseCaseImpl implements MerchantGetUseCase {
+
+    private final MerchantRepository merchantRepository;
+    private final MerchantMappers merchantMappers;
+
+    @Override
+    public List<MerchantOutputDTO> getByName(String name) {
+
+        List<Merchant> res = merchantRepository.findByName(name);
+
+        // Transformación a DTO
+        List<MerchantOutputDTO> merchantOutputDTOList = res.stream()
+                .map(merchantMappers::merchantToOutput)
+                .collect(Collectors.toList());
+
+        return merchantOutputDTOList;
+
+    }
+
+    @Override
+    public MerchantOutputDTO getById(String id) {
+
+        Merchant merchant = merchantRepository.findById(id);
+
+        // Transformación a DTO
+        MerchantOutputDTO res = merchantMappers.merchantToOutput(merchant);
+
+        return res;
+
+    }
+
 }
--- src/main/java/com/sarawipay/merchant_microservice/Merchant/application/port/MerchantGetUseCase.java
+++ src/main/java/com/sarawipay/merchant_microservice/Merchant/application/port/MerchantGetUseCase.java
@@ -1,4 +1,13 @@
 package com.sarawipay.merchant_microservice.Merchant.application.port;
 
+import com.sarawipay.merchant_microservice.Merchant.infrastructure.controller.DTO.output.MerchantOutputDTO;
+
+import java.util.List;
+
 public interface MerchantGetUseCase {
+
+    List<MerchantOutputDTO> getByName(String name);
+
+    MerchantOutputDTO getById(String id);
+
 }
