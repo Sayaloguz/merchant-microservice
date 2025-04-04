6105eea0378c814188afa5dc62ba9ed22472559e
sara-lopez <sara.lopez@petroprix.com>
2025-04-04 11:15:32 +0200

    fix: [MAPPER]: Arreglar mapper

--- src/main/java/com/sarawipay/merchant_microservice/Merchant/domain/mappers/MerchantMappers.java
+++ src/main/java/com/sarawipay/merchant_microservice/Merchant/domain/mappers/MerchantMappers.java
@@ -5,8 +5,12 @@ import com.sarawipay.merchant_microservice.Merchant.infrastructure.controller.DT
 import com.sarawipay.merchant_microservice.Merchant.infrastructure.controller.DTO.output.MerchantOutputDTO;
 import org.mapstruct.Mapper;
 
+
 @Mapper(componentModel = "spring")
 public interface MerchantMappers {
-    Merchant inputToClient(MerchantInputDTO merchantInputDTO);
-    MerchantOutputDTO clientToOutput(Merchant merchant);
+
+    Merchant inputToMerchant(MerchantInputDTO merchantInputDTO);
+
+    MerchantOutputDTO merchantToOutput(Merchant merchant);
+
 }
