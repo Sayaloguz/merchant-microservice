31190f0a420ef95283b782a09120ca079fac681d
sara-lopez <sara.lopez@petroprix.com>
2025-04-04 11:12:45 +0200

    feat: [SERVICE]: Implementar servicio para actualizar un merchant

--- src/main/java/com/sarawipay/merchant_microservice/Merchant/application/MerchantUpdateUseCaseImpl.java
+++ src/main/java/com/sarawipay/merchant_microservice/Merchant/application/MerchantUpdateUseCaseImpl.java
@@ -1,4 +1,33 @@
 package com.sarawipay.merchant_microservice.Merchant.application;
 
-public class MerchantUpdateUseCaseImpl {
+import com.sarawipay.merchant_microservice.Merchant.application.port.MerchantUpdateUseCase;
+import com.sarawipay.merchant_microservice.Merchant.domain.Merchant;
+import com.sarawipay.merchant_microservice.Merchant.domain.mappers.MerchantMappers;
+import com.sarawipay.merchant_microservice.Merchant.infrastructure.controller.DTO.input.MerchantInputDTO;
+import com.sarawipay.merchant_microservice.Merchant.infrastructure.controller.DTO.output.MerchantOutputDTO;
+import com.sarawipay.merchant_microservice.Merchant.infrastructure.controller.repository.port.MerchantRepository;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Service
+public class MerchantUpdateUseCaseImpl implements MerchantUpdateUseCase {
+
+    private final MerchantRepository merchantRepository;
+    private final MerchantMappers merchantMappers;
+
+    @Override
+    public MerchantOutputDTO update(MerchantInputDTO merchantInputDTO, String pk, String sk) {
+
+        Merchant merchant = merchantRepository.update(merchantInputDTO, pk, sk);
+        MerchantOutputDTO merchantOutputDTO = merchantMappers.merchantToOutput(merchant);
+
+        return merchantOutputDTO;
+
+    }
+
 }
--- src/main/java/com/sarawipay/merchant_microservice/Merchant/application/port/MerchantUpdateUseCase.java
+++ src/main/java/com/sarawipay/merchant_microservice/Merchant/application/port/MerchantUpdateUseCase.java
@@ -1,4 +1,10 @@
 package com.sarawipay.merchant_microservice.Merchant.application.port;
 
+import com.sarawipay.merchant_microservice.Merchant.infrastructure.controller.DTO.input.MerchantInputDTO;
+import com.sarawipay.merchant_microservice.Merchant.infrastructure.controller.DTO.output.MerchantOutputDTO;
+
 public interface MerchantUpdateUseCase {
+
+    MerchantOutputDTO update(MerchantInputDTO merchantInputDTO, String pk, String sk);
+
 }
